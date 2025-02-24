81a7c5004a9cc39622f0a1220aff3fa713fb4b13
Rafiita28 <158265241+Rafiita28@users.noreply.github.com>
2025-02-24 12:14:32 +0100

    Update AreaCalculator.java

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -10,12 +10,5 @@ public class AreaCalculator {
     public static double circleArea(double radius) {
         return Math.PI * radius * radius;
     }
-
-    public static double rectangleArea(double base, double height) {
-        return base * height;
-    }
-
-    public static double triangleArea(double base, double height) {
-        return (base * height) / 2;
-    }
+    // Añadir métodos para calcular el área de un rectángulo y un triángulo
 }
